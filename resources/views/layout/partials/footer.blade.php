7e7ae9c34ac7852d856ea7a8773c02cda83c5ff1
Daniel Leach <dleach@kimmel.com>
2017-05-26 16:47:58 -0400

    Adding jquery.

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -3,6 +3,7 @@
         <p class="disclaimer">This site is fan-made and not officially affiliated with Big Picture Games.</p>
         <p class="copyright">&copy; Copyright 2017</p>
     </footer>
+    <script src="/js/jquery-3.2.1.min.js"></script>
     @yield('scripts')
 </body>
 </html>
