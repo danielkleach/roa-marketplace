2447aaaf9b81dc4a8204e40d2cd5d66224456fa5
Daniel Leach <danielkleach@gmail.com>
2017-06-11 21:33:05 -0400

    Adding Race model.

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -19,6 +19,7 @@
                             <th>Quantity</th>
                             <th>Price Each</th>
                             <th>Created</th>
+                            <th>Actions</th>
                         </tr>
                         @if (count($sellOrders))
                             @foreach ($sellOrders as $order)
@@ -28,6 +29,7 @@
                                     <td class="text-right">{{ number_format($order->quantity) }}</td>
                                     <td class="text-right">{{ number_format($order->price) }}G</td>
                                     <td>{{ $order->formattedDate }}</td>
+                                    <td><button type="button" class="btn btn-danger" data-id="{{ $order->id }}" onclick="closeOrder(this)">Close Order</button></td>
                                 </tr>
                             @endforeach
                         @else
@@ -41,6 +43,7 @@
                             <th>Quantity</th>
                             <th>Price Each</th>
                             <th>Created</th>
+                            <th>Actions</th>
                         </tr>
                     </table>
                 </div>
@@ -55,6 +58,7 @@
                             <th>Quantity</th>
                             <th>Price Each</th>
                             <th>Created</th>
+                            <th>Actions</th>
                         </tr>
                         @if (count($buyOrders))
                             @foreach ($buyOrders as $order)
@@ -64,6 +68,7 @@
                                     <td class="text-right">{{ number_format($order->quantity) }}</td>
                                     <td class="text-right">{{ number_format($order->price) }}G</td>
                                     <td>{{ $order->formattedDate }}</td>
+                                    <td><button type="button" class="btn btn-danger" data-id="{{ $order->id }}" onclick="closeOrder(this)">Close Order</button></td>
                                 </tr>
                             @endforeach
                         @else
@@ -77,10 +82,104 @@
                             <th>Quantity</th>
                             <th>Price Each</th>
                             <th>Created</th>
+                            <th>Actions</th>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <h4 class="text-white">Expired Orders</h4>
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <tr>
+                            <th>Item</th>
+                            <th>Type</th>
+                            <th>Location</th>
+                            <th>Quantity</th>
+                            <th>Price Each</th>
+                            <th>Expired</th>
+                            <th>Actions</th>
+                        </tr>
+                        @if (count($expiredOrders))
+                            @foreach ($expiredOrders as $order)
+                                <tr>
+                                    <td><a href="{{ route('orders.show', $order->id) }}">{{ $order->item->name }}</a></td>
+                                    <td>{{ $order->type }}</td>
+                                    <td>{{ $order->location->name }}</td>
+                                    <td class="text-right">{{ number_format($order->quantity) }}</td>
+                                    <td class="text-right">{{ number_format($order->price) }}G</td>
+                                    <td>{{ $order->expiredDate }}</td>
+                                    <td><button type="button" class="btn btn-primary" data-id="{{ $order->id }}" onclick="refreshOrder(this)">Refresh Order</button></td>
+                                </tr>
+                            @endforeach
+                        @else
+                            <tr>
+                                <td class="text-center" colspan="5">This user has no expired orders.</td>
+                            </tr>
+                        @endif
+                        <tr>
+                            <th>Item</th>
+                            <th>Type</th>
+                            <th>Location</th>
+                            <th>Quantity</th>
+                            <th>Price Each</th>
+                            <th>Expired</th>
+                            <th>Actions</th>
                         </tr>
                     </table>
                 </div>
             </div>
         </div>
     </main>
+@endsection
+
+@section('scripts')
+    <script src="/js/moment.min.js"></script>
+    <script>
+        function closeOrder(order) {
+            var id = order.dataset.id;
+            var endDate = moment().format('YYYY-MM-DD HH:mm:ss');
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-Token': $('input[name="_token"]').val()
+                }
+            });
+            $.ajax({
+                type: "PATCH",
+                url: `/orders/${id}`,
+                data: {
+                    end_date: endDate
+                },
+                success: function (data) {
+                    window.location.reload(true);
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    console.log('Error' + thrownError);
+                }
+            });
+        }
+
+        function refreshOrder(order) {
+            var id = order.dataset.id;
+            var endDate = moment().add(3, 'days').format('YYYY-MM-DD hh:mm:ss');
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-Token': $('input[name="_token"]').val()
+                }
+            });
+            $.ajax({
+                type: "PATCH",
+                url: `/orders/${id}`,
+                data: {
+                    end_date: endDate
+                },
+                success: function (data) {
+                    window.location.reload(true);
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    console.log('Error' + thrownError);
+                }
+            });
+        }
+    </script>
 @endsection
