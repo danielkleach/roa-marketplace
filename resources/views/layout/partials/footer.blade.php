3fc686eca341f9946ddab7598a2a98d41347d48a
Daniel Leach <dleach@kimmel.com>
2017-06-22 10:41:52 -0400

    Cleaning up javascript files and making use of cdns for vendor files.

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -3,14 +3,16 @@
         <p class="disclaimer">This site is fan-made and not officially affiliated with Big Picture Games.</p>
         <p class="copyright">&copy; Copyright 2017 <em>(v.1)</em></p>
     </footer>
-    <script src="/js/jquery-3.2.1.min.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/bootstrap-select.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
     <script src="https://cdn.jsdelivr.net/instantsearch.js/1/instantsearch.min.js"></script>
     <script>
         var algoliaKey = '{{ config('app.algolia_key') }}';
         var environment = '{{ config('app.env') }}';
     </script>
-    <script src="/js/search.js"></script>
+    <script src="/js/app.js"></script>
     @yield('scripts')
 </body>
 </html>
