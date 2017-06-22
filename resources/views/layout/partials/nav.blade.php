a2396b12479669ad1b1b2a9b6d07690921653d69
Daniel Leach <dleach@kimmel.com>
2017-06-22 16:19:57 -0400

    Updating views.

--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -1,45 +1,35 @@
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-4 col-sm-12">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="{{ route('home.index') }}">Home</a></li>
-                    <li><a href="{{ route('orders.create') }}">Buy/Sell</a></li>
-                    {{--<li><a href="#">Contact</a></li>--}}
-                </ul>
-            </div>
-            @include('layout.partials.search')
-            <div class="col-md-4 col-sm-12 pull-right">
-                <ul class="nav navbar-nav navbar-right">
-                    @if (Auth::guest())
-                        <li class="border-right"><a href="{{ route('register') }}">Register</a></li>
-                        <li><a href="{{ route('login') }}">Login</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->profile->character_name }} <span class="caret"></span>
-                            </a>
+<nav class="container">
+    <a class="logo pull-left" href="{{ route('home.index') }}">ROA Marketplace</a>
+    <ul class="nav nav-left pull-left">
+        <li><a href="{{ route('orders.create') }}">Buy/Sell</a></li>
+    </ul>
+    <ul class="nav nav-right pull-right">
+        @if (Auth::guest())
+            <li><a href="{{ route('register') }}">Register</a></li>
+            <li><a href="{{ route('login') }}">Login</a></li>
+        @else
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                    {{ Auth::user()->profile->character_name }} <span class="caret"></span>
+                </a>
 
-                            <ul class="dropdown-menu" role="menu">
-                                <li>
-                                    <a href="{{ route('profiles.show', Auth::user()->profile->id) }}">Profile</a>
-                                </li>
-                                <li>
-                                    <a href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
+                <ul class="dropdown-menu" role="menu">
+                    <li>
+                        <a href="{{ route('profiles.show', Auth::user()->profile->id) }}">Profile</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                     document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </li>
-                            </ul>
-                        </li>
-                    @endif
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </li>
                 </ul>
-            </div>
-        </div>
-    </div>
+            </li>
+        @endif
+    </ul>
 </nav>
--- resources/views/layout/partials/search.blade.php
+++ resources/views/layout/partials/search.blade.php
@@ -1,5 +1,3 @@
-<div class="col-md-4 col-sm-12">
-    <form class="navbar-form search">
-            <input type="text" id="search-input" class="form-control" placeholder="Search">
-    </form>
-</div>
+ <form class="navbar-form search">
+     <input type="text" id="search-input" class="form-control" placeholder="Search">
+ </form>
